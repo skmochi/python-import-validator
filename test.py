1c5790a523fce22fd4e1f7462852357d4e102ea7
sk-mochi <yuki.sakamoto@ntt.com>
2023-02-06 15:08:16 +0900

    fix

--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-import pandas as pandas
+from dataclasses import dataclass, field
 
-pandas.compat._optional.import_optional_dependency("requests")
+@dataclass
+class Karaage():
+    name: str
+    num: int
+
+lst = []
+k1 = Karaage(name="onamae1", num=1)
+k2 = Karaage(name="onamae2", num=2)
+
+lst = [k1, k2]
+
+print(lst["onamae1"])
--- validator.py
+++ validator.py
@@ -1,8 +1,11 @@
 """
-from A.B import X, Yの場合、
-rootlib: A
-subs: B, X, Y
-と定義
+1. import系は全て弾く(システム側で付与する方針)
+2. メソッド呼び出しは許可されたlibraryとasnameの組み合わせのみ
+e.g.
+library: numpy
+asname: np
+method: min, max, round
+許可パターン: {parent: np or numpy, method: [min, max, round]}
 """
 
 # ref: https://gist.github.com/jtpio/cb30bca7abeceae0234c9ef43eec28b4
@@ -16,39 +19,15 @@ WHITELIST_FILEPATH = "./whitelist.yaml"
 
 
 # TODO: リファクタリング
-class Whitelist():
-    def __init__(self, filepath):
-        """
-        Return: [{'rootlib': 'numpy', 'subs': ['max', 'min', 'round']}]
-        """
-        with open(filepath) as file:
-            obj = yaml.safe_load(file)
-        # self.whitelist: list[dict] = obj['whitelist']
-        self.whitelist = [WhitelistInfo(**x) for x in obj['whitelist']]
+def load_whitelist(filepath):
+    with open(filepath) as file:
+        obj = yaml.safe_load(file)
+    whitelist = [WhitelistInfo(**x) for x in obj['whitelist']]
+    return whitelist
 
-    def whitelist_rootlib(self) -> set[str]:
-        """
-        whitelistのrootlibのsetを返す
-        """
-        whitelist_rootlib = [x.rootlib for x in self.whitelist]
-        return set(whitelist_rootlib)
-
-    def whitelist_subs(self, rootlib) -> set:
-        """
-        whitelistの指定rootlibのsubsのsetを返す
-        """
-        whitelist_subs = [x.subs for x in self.whitelist if x.rootlib == rootlib]
-        if whitelist_subs:  # もし指定rootlibがwhitelist内にあれば
-            whitelist_subs:list[str] = whitelist_subs[0]
-        return set(whitelist_subs)
 
 @dataclasses.dataclass
 class WhitelistInfo:
-    rootlib: str
-    subs: list
-
-@dataclasses.dataclass
-class ImportInfo:
     rootlib: str
     subs: list
     asname: str = None
@@ -69,146 +48,42 @@ class CallInfo:
 
 class CustomVisitor(ast.NodeVisitor):
     def __init__(self):
-        """
-        import_info: list(dict(string)) =
-        [{'rootlib': 'numpy', 'subs': None, 'asname': 'np'}, {'rootlib': 'pandas', 'subs': 'to_csv', 'asname': 'csv'}]
-        """
-        self.imported_rootlib: set[str] = set()
-        self.imported_info: list[ImportInfo] = list()
         self.call_info: list[CallInfo] = list()
-        self.whitelist = Whitelist(filepath=WHITELIST_FILEPATH)
+        self.whitelist = load_whitelist(filepath=WHITELIST_FILEPATH)
 
     def visit_Import(self, node):
         """
         import X を検出するたびに実行される関数
         バリデーションに必要なデータ構造を構築
         """
-        print("visit_Import")
-        # import os, sys の場合、node.namesの要素は2つ
-        for x in node.names:
-            libname = x.name
-            # import X.Y.Z のようなパターンに対応
-            # rootlib: X, subs: [Y, Z] とする
-            rootlib: str = libname.split(".")[0]
-            subs: list[str] = libname.split(".")[1:]
-            self.imported_rootlib.add(rootlib)
-            asname: str | None = getattr(x, "asname", None)
-            d = ImportInfo(rootlib=rootlib, subs=list(subs), asname=asname)
-            self.imported_info.append(d)
-        self.generic_visit(node)
+        raise ValueError("import is bannd!!")
+        # TODO: 必要かよくわからん
+        # self.generic_visit(node)
+
 
     def visit_ImportFrom(self, node):
         """
         from X import Y を検出するたびに実行される関数
         from rootlib import subs
         """
-        print("visit_ImportFrom")
-        subs = set()
-        libname = node.module
-        rootlib: str = libname.split(".")[0]
-        self.imported_rootlib.add(rootlib)
-        _subs: list[str] = libname.split(".")[1:]  # .でバラす
-        subs = subs | set(_subs)
-        # from X import Y のY部分をsubsに格納
-        # from numpy import min, max as np の場合、node.namesはminとmaxの分の2つ
-        asname = []
-        for x in node.names:
-            subs.add(x.name)
-            _asname = getattr(x, "asname", None)
-            if _asname is not None:
-                asname.append(_asname)
-        if len(asname) == 1:
-            asname = asname[0]
-        elif len(asname) == 0:
-            asname = None
-        else:
-            # どうせpython実行時にエラーが出ると思うけど念のため確認
-            raise ValueError("as nameは1つのimportで2つ以上の定義はできません")
-        d = ImportInfo(rootlib=rootlib, subs=list(subs), asname=asname)
-        self.imported_info.append(d)
-        self.generic_visit(node)
+        raise ValueError("import is bannd!!")
+        # TODO: 必要かよくわからん
+        # self.generic_visit(node)
 
     def visit_Call(self, node):
-        print(vars(node.func.value))
-        # import A
-        # A.B.C.method()
-        # 上記ケースだとnode.func.value内にさらにvalueがあり...の構造をとる
-        # -> [method[C[B[A]]]]のような雰囲気
+        # print(vars(node.func.value))
+        # A.B.C.method()のパターンの呼び出しは許可リストの存在しないので弾く
+        if not hasattr(node.func.value, "id"):
+            raise ValueError("call1: 許可されていないメソッドの呼び出しです")
         d = CallInfo(parent=node.func.value.id, attr=node.func.attr)
-        print(d)
-        self.call_info.append(d)
-        self.generic_visit(node)
-
-    def check_rootlib(self):
-        """
-        - rootlibがwhitelistにあるか確認
-        """
-        whitelist_rootlib = self.whitelist.whitelist_rootlib()
-        diff = self.imported_rootlib - whitelist_rootlib
-        if len(diff) == 0:
-            return
-        else:
-            raise ValueError(f"imported bannd library: {list(diff)[0]}")
-
-    def check_subs(self):
-        """
-        - subsがwhitelistにあるか確認
-        """
-        for info in self.imported_info:
-            rootlib = info.rootlib
-            subs = info.subs
-            whitelist_subs = self.whitelist.whitelist_subs(rootlib)
-            diff = set(subs) - whitelist_subs
-            if len(diff) > 0:
-                raise ValueError(f"imported bannd method: {list(diff)[0]}")
-
-    def check_calls(self):
-        """
-        - asnameとの整合性をチェックしつつCallされたmethodがwhitelistにあるか確認
-        方針: まずはcallのrootlibを特定し、これがwhitelist_subsに含まれているかを確認する
-        """
-        for call_info in self.call_info:
-            parent = call_info.parent
-            attr = call_info.attr
-            # TODO: このへん適当
-            # parentがasnameと仮定して走査しrootlibを割り出す
-            _rootlib_from_asname = [x.rootlib for x in self.imported_info if parent in x.asname]
-            # parentがsubsと仮定して走査しrootlibを割り出す
-            _rootlib_from_subs = [x.rootlib for x in self.imported_info if parent in x.subs]
-            # parentがrootlibと仮定して走査しrootlibを割り出す
-            _rootlib_from_rootlib = [x.rootlib for x in self.imported_info if parent in x.rootlib]
-            sumset = set(_rootlib_from_asname) | set(_rootlib_from_subs) | set(_rootlib_from_rootlib)
-            # これも必要かよくわからん
-            if len(sumset) > 1:
-                raise ValueError("asnameの命名を変えてください")
-            if _rootlib_from_asname:
-                for info in self.imported_info:
-                    if info.asname == _rootlib_from_asname[0]:
-                        rootlib = info.rootlib  # rootlibを特定
-                        if attr not in self.whitelist.whitelist_subs(rootlib):
-                            raise ValueError("禁止されているものが使用されています")
-            if _rootlib_from_subs:
-                for info in self.imported_info:
-                    if info.asname == _rootlib_from_subs[0]:
-                        rootlib = info.rootlib  # rootlibを特定
-                        if attr not in self.whitelist.whitelist_subs(rootlib):
-                            raise ValueError("禁止されているものが使用されています")
-            if _rootlib_from_rootlib:
-                for info in self.imported_info:
-                    if info.asname == _rootlib_from_rootlib[0]:
-                        rootlib = info.rootlib  # rootlibを特定
-                        if attr not in self.whitelist.whitelist_subs(rootlib):
-                            raise ValueError("禁止されているものが使用されています")
-        return
-
-
-    def check_asnane_independencies(self):
-        # TODO: import_info内にasname同士の被りがないか確認
-        # asnameの被りがあると紐付けが困難になるので弾く
-        asnames = [x.asname for x in self.imported_info if x.asname is not None]
-        print(asnames)
-        if len(asnames) != len(set(asnames)):
-            raise ValueError("asで定義する名前は他のものと被らないようにしてください")
+        # このdが許可リスト内にあるか確認する(rootlib or asnameが一致していればok)
+        # 一致するparentを検索
+        _pass = [x for x in self.whitelist if d.parent in (x.asname, x.rootlib)]
+        for x in _pass:
+            if d.attr not in x.subs:
+                raise ValueError("This method is banned.")
+        # TODO: 必要かよくわからん
+        # self.generic_visit(node)
 
 
 code1 = """
@@ -232,9 +107,10 @@ np.max("AAA")
 """
 
 code = """
-import pandas as pandas
+# import pandas as pandas
 
-pandas.compat._optional.import_optional_dependency("requests")
+# pandas.compat._optional.import_optional_dependency("requests")
+numpy.round(1)
 """
 
 tree = ast.parse(code)
@@ -243,7 +119,3 @@ tree = ast.parse(code)
 
 visitor = CustomVisitor()
 visitor.visit(tree)  # def visitXXX()を全て実行
-visitor.check_asnane_independencies()
-visitor.check_rootlib()
-visitor.check_subs()
-visitor.check_calls()
